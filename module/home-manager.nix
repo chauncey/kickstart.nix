49dd9db78f9b1e61028bcf3ad6e4a37174bc2689
chauncey <cchauncey@gmail.com>
2024-03-04 20:51:33 -0600

    Add to home manager

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -6,12 +6,77 @@
 
   programs.tmux = {
     enable = true;
+    extraConfig = builtins.readFile ../config/tmux/.tmux.conf;
   };
 
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    colors = {
+      bg = "#24273a";
+      "bg+" = "#363a4f";
+      spinner = "#f4dbd6";
+      hl = "#ed8796";
+      fg = "#cad3f5";
+      header = "#ed8796";
+      info = "#c6a0f6";
+      pointer = "#f4dbd6";
+      marker = "#f4dbd6";
+      "fg+" = "#cad3f5";
+      prompt = "#c6a0f6";
+      "hl+" = "#ed8796";
+    };
+    defaultOptions = [
+      "--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
+      "--preview 'cat {}'"
+    ];
   };
 
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      aws = {
+        disabled = true;
+      };
+      buf = {
+        disabled = true;
+      };
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[>](bold red)";
+      };
+      directory = {
+        truncate_to_repo = false;
+      };
+      dotnet = {
+        detect_files = [
+          "global.json"
+          "Directory.Build.props"
+          "Directory.Build.targets"
+          "Packages.props"
+        ];
+      };
+      git_branch = {
+        symbol = " ";
+        truncation_length = 18;
+      };
+      golang = {
+        symbol = " ";
+      };
+      lua = {
+        symbol = " ";
+      };
+      nix_shell = {
+        symbol = " ";
+      };
+      package = {
+        disabled = true;
+      };
+    };
+  };
+  programs.less = {
+    enable = true;
+  };
 
 }
